c50000a6af34ee9a2cf2a7dff5090bb9eb6946d9
Chris Maguire <cwmaguire@protonmail.com>
2025-02-01 16:31:32 -0800

    Code cleanup

--- priv/static/how_erl.js
+++ priv/static/how_erl.js
@@ -1,4 +1,4 @@
-var urls;
+const RESIZE_CORNER_SIZE = 20;
 const MIN_URL_SIZE = 70;
 
 document.ondblclick = create_new_url;
@@ -12,28 +12,13 @@ async function getData() {
     }
 
     const json = await response.json();
-    create_urls(json);
+    json.forEach(create_url);
   } catch (error) {
     console.error(error.message);
   }
 }
 
-function post_url_data() {
-  const urlData = query_url_data();
-  console.log(`Writing data: ${urlData}`);
-  const url = "http://main:8080/video_urls";
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(urlData),
-    headers: { "Content-Type": "application/json" }
-  });
-}
-
 getData();
-
-function create_urls(json){
-  json.forEach(create_url);
-}
 
 function create_new_url({pageX: x, pageY: y}){
   const urlData = {
@@ -49,36 +34,17 @@ function create_new_url({pageX: x, pageY: y}){
   post_url_data();
 }
 
-function copy_url(urlData){
-  const Y_BUFFER = 20;
-  const newUrlData = structuredClone(urlData);
-  newUrlData.left = newUrlData.left + newUrlData.w + Y_BUFFER;
-  render_url(newUrlData);
-  post_url_data();
-}
-
-function delete_url(elem){
-  elem.remove();
-  post_url_data();
-}
-
 function create_url(urlData){
-  render_url(urlData);
-}
-
-function render_url(urlData){
-  const {name, desc, url, top, left, w, h} = urlData;
-
   const mainDiv = create_main_div(urlData);
   render_content(mainDiv);
   create_resize_corner_divs(mainDiv);
-
   return mainDiv;
 }
 
 function create_main_div(urlData){
-  const {name, desc, url, top, left, w, h} = urlData;
+  const {top, left, w, h} = urlData;
   const div = document.createElement("div");
+  div.urlData = urlData;
   div.style.position = "fixed";
   div.style.border = "1px solid black";
   div.style.borderRadius = "15px";
@@ -88,18 +54,14 @@ function create_main_div(urlData){
   div.style.height = h + 'px';
   div.style.padding = '10px';
   div.className = 'url-div';
-  div.urlData = urlData;
 
+  div.onmousedown = ({pageX: x, pageY: y}) => follow_mouse(div, {x, y});
+  div.moveHandlers = [move_to, move_resizers, update_to];
   div.upHandlers = [function() {
                       div.style.zIndex = 0;
                     }];
-  div.moveHandlers = [move_to, move_resizers, update_to];
-
-  document.body.appendChild(div);
-
-  div.onmousedown = ({pageX: x, pageY: y}) => follow_mouse(div, {x, y});
   div.ondblclick = edit_url;
-
+  document.body.appendChild(div);
   return div;
 }
 
@@ -107,30 +69,39 @@ function render_content(elem){
   const {urlData} = elem;
   const {name, desc, url} = urlData;
   elem.innerHTML = `<a href='${url}'>${name}</a><br>${desc}<br>`;
+  button(elem, 'copy', copy_url);
+  button(elem, 'delete', delete_url);
+}
 
-  const copyButton = document.createElement("button");
-  copyButton.textContent = 'copy';
-  copyButton.style.margin = '5px';
-  copyButton.onclick =
+function button(elem, text, onClick){
+  const b = document.createElement("button");
+  b.textContent = text;
+  b.style.margin = '5px';
+  b.onclick =
     (event) => {
       event.stopPropagation();
-      copy_url(urlData);
+      onClick(elem);
     };
-  copyButton.onmousedown = stop_propagation;
-  copyButton.onmousemove = stop_propagation;
-  elem.appendChild(copyButton);
+  b.onmousedown = stop_propagation;
+  b.onmousemove = stop_propagation;
+  elem.appendChild(b);
+}
 
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = 'delete';
-  deleteButton.style.margin = '5px';
-  deleteButton.onclick =
-    (event) => {
-      event.stopPropagation();
-      delete_url(elem);
-    };
-  deleteButton.onmousedown = stop_propagation;
-  deleteButton.onmousemove = stop_propagation;
-  elem.appendChild(deleteButton);
+function copy_url({urlData}){
+  const Y_BUFFER = 20;
+  const newUrlData = structuredClone(urlData);
+  newUrlData.left = newUrlData.left + newUrlData.w + Y_BUFFER;
+  create_url(newUrlData);
+  post_url_data();
+}
+
+function delete_url(elem){
+  elem.topLeft.remove();
+  elem.topRight.remove();
+  elem.bottomLeft.remove();
+  elem.bottomRight.remove();
+  elem.remove();
+  post_url_data();
 }
 
 function float_above_everything(elem){
@@ -142,9 +113,9 @@ function follow_mouse(elem, {x, y}){
   elem.mouseDelta = {xd, yd};
   float_above_everything(elem);
   const moveHandlers = elem.moveHandlers;
-  const upHandlers = elem.upHandlers.slice();
-  upHandlers.unshift(stop_following_mouse);
-  upHandlers.unshift(post_url_data);
+  const upHandlers = [stop_following_mouse,
+                      post_url_data,
+                      ...elem.upHandlers];
   document.onmousemove = apply_event_handlers_fun(elem, moveHandlers);
   document.onmouseup = apply_event_handlers_fun(elem, upHandlers);
 }
@@ -160,11 +131,10 @@ function stop_following_mouse(){
     document.onmouseup = null;
 }
 
-function move_to(elem, {x, y}) {
+function move_to(elem, {x: mouseX, y: mouseY}) {
   const {xd, yd} = elem.mouseDelta;
-  //console.log(`move_to x: ${x}, y: ${y}, xd: ${xd}, yd: ${yd}`);
-  elem.style.left = x + xd + 'px';
-  elem.style.top = y + yd + 'px';
+  elem.style.left = mouseX + xd + 'px';
+  elem.style.top = mouseY + yd + 'px';
 }
 
 function move_resizers(elem){
@@ -189,16 +159,7 @@ function update_to(elem){
 function mouse_delta({style: {top, left}}, x, y){
   const xd = i(left) - x;
   const yd = i(top) - y;
-  //console.log(`mouse_delta: top ${top} left ${left} x ${x} y ${y} xd ${xd} yd ${yd}`);
   return {xd, yd};
-}
-
-function center(elem, x, y){
-  const halfWidth = Math.round(elem.offsetWidth / 2);
-  const halfHeight = Math.round(elem.offsetHeight / 2);
-  const xCenter = (x - halfWidth);
-  const yCenter = (y - halfHeight);
-  return {xCenter: xCenter, yCenter: yCenter};
 }
 
 function create_resize_corner_divs(elem){
@@ -207,6 +168,32 @@ function create_resize_corner_divs(elem){
   elem.topRight = create_resize_corner_div(cps.tr, elem);
   elem.bottomLeft = create_resize_corner_div(cps.bl, elem);
   elem.bottomRight = create_resize_corner_div(cps.br, elem);
+}
+
+function create_resize_corner_div({x, y, is_top, is_left}, parentDiv){
+  const halfSize = i(RESIZE_CORNER_SIZE / 2);
+  const top = y - halfSize;
+  const left = x - halfSize;
+  const cursor = get_cursor(is_top, is_left);
+
+  const div = document.createElement("div");
+  div.style.position = "fixed";
+  div.style.border = "1px dotted #C0C0C0";
+  div.style.top = top + 'px';
+  div.style.left = left + 'px';
+  div.style.width = RESIZE_CORNER_SIZE + 'px';
+  div.style.height = RESIZE_CORNER_SIZE + 'px';
+  div.style.cursor = cursor;
+  div.className = 'url-resize-div';
+  div.parentDiv = parentDiv;
+  div.is_top = is_top;
+  div.is_left = is_left;
+  div.moveHandlers = [resize_parent];
+  div.upHandlers = [];
+  div.mouseDelta = {xd: -10, yd: -10};
+  div.onmousedown = ({pageX: x, pageY: y}) => follow_mouse(div, {x, y});
+  document.body.appendChild(div);
+  return div;
 }
 
 function corner_points({offsetWidth: w,
@@ -227,38 +214,6 @@ function corner_points({offsetWidth: w,
   const br = {x: r, y: b, is_top: false, is_left: false};
 
   return {tl: tl, tr: tr, bl: bl, br: br};
-}
-
-function create_resize_corner_div({x, y, is_top, is_left}, parentDiv){
-
-  const halfSize = 10;
-  const top = y - halfSize;
-  const left = x - halfSize;
-
-  const cursor = get_cursor(is_top, is_left);
-
-  const div = document.createElement("div");
-  div.style.position = "fixed";
-  div.style.border = "1px dotted red";
-  div.style.top = top + 'px';
-  div.style.left = left + 'px';
-  div.style.width = (halfSize * 2) + 'px';
-  div.style.height = (halfSize * 2) + 'px';
-  div.style.cursor = cursor;
-  div.className = 'url-resize-div';
-
-  div.parentDiv = parentDiv;
-  div.is_top = is_top;
-  div.is_left = is_left;
-  div.moveHandlers = [resize_parent];
-  div.upHandlers = [];
-  div.mouseDelta = {xd: -10, yd: -10};
-
-  document.body.appendChild(div);
-
-  div.onmousedown = ({pageX: x, pageY: y}) => follow_mouse(div, {x, y});
-
-  return div;
 }
 
 function get_cursor(is_top, is_left){
@@ -294,6 +249,18 @@ function resize_parent_({parentDiv}, event, dimsFun){
   update(parentDiv, clampedDims);
 }
 
+function update(elem, {t, l, h, w}){
+    elem.style.height = h + 'px';
+    elem.style.width = w + 'px';
+    elem.style.top = t + 'px';
+    elem.style.left = l + 'px';
+
+    elem.urlData.h = h;
+    elem.urlData.w = w;
+    elem.urlData.top = t;
+    elem.urlData.left = l;
+}
+
 function dimensions({style: {top: t, left: l, width: w, height: h}}){
   return {t: i(t), l: i(l), w: i(w), h: i(h)};
 }
@@ -318,10 +285,6 @@ function calc_div_h_w_bottom_right({t, l, w, h}, {x, y}){
   return {t: t, l: l, h: y - t, w: x - l};
 }
 
-function is_too_small({h, w}){
-  return h < MIN_URL_SIZE || w < MIN_URL_SIZE;
-}
-
 function clamp_dims({t: t2, l: l2, h: h2, w: w2},
                     {t: t1, l: l1, h: h1, w: w1}){
   const clamped = {t: t2, l: l2, h: h2, w: w2};
@@ -340,18 +303,6 @@ function i(s){
   return Number.parseInt(s);
 }
 
-function update(elem, {t, l, h, w}){
-    elem.style.height = h + 'px';
-    elem.style.width = w + 'px';
-    elem.style.top = t + 'px';
-    elem.style.left = l + 'px';
-
-    elem.urlData.h = h;
-    elem.urlData.w = w;
-    elem.urlData.top = t;
-    elem.urlData.left = l;
-}
-
 function query_url_data(){
   const notArray = document.querySelectorAll(".url-div");
   const nodes = Array.from(notArray);
@@ -363,38 +314,10 @@ function edit_url(event){
   const elem = event.target;
   const {desc, url, name} = elem.urlData;
 
-  const nameInput = document.createElement('input');
-  nameInput.id = 'nameInput';
-  nameInput.type = 'text';
-  nameInput.size = '20';
-  nameInput.value = name;
-  nameInput.onmousedown = (event) => event.stopPropagation();
-
-  const nameLabel = document.createElement('label');
-  nameLabel.htmlFor = 'nameInput';
-  nameLabel.innerText = 'Name: ';
-
-  const descInput = document.createElement('input');
-  descInput.id = 'descInput';
-  descInput.type = 'text';
-  descInput.size = '20';
-  descInput.value = desc;
-  descInput.onmousedown = (event) => event.stopPropagation();
-
-  const descLabel = document.createElement('label');
-  descLabel.htmlFor = 'descInput';
-  descLabel.innerText = 'Description: ';
-
-  const urlInput = document.createElement('input');
-  urlInput.id = 'urlInput';
-  urlInput.type = 'text';
-  urlInput.size = '20';
-  urlInput.value = url;
-  urlInput.onmousedown = stop_propagation;
-
-  const urlLabel = document.createElement('label');
-  urlLabel.htmlFor = 'urlInput';
-  urlLabel.innerText = 'URL: ';
+  elem.replaceChildren();
+  const nameInput = add_textbox(elem, 'nameInput', name, 'Name: ');
+  const descInput = add_textbox(elem, 'descInput', desc, 'Desc: ');
+  const urlInput = add_textbox(elem, 'urlInput', url, 'URL: ');
 
   const saveButton = document.createElement('button');
   saveButton.urlInput = urlInput;
@@ -404,27 +327,34 @@ function edit_url(event){
   saveButton.onclick = (event) => save(event, elem);
   saveButton.onmousedown = stop_propagation;
   saveButton.onmousemove = stop_propagation;
+  elem.appendChild(saveButton);
 
   const cancelButton = document.createElement('button');
   cancelButton.textContent = 'cancel';
   cancelButton.onclick = (event) => cancel(event, elem);
   cancelButton.onmousedown = stop_propagation;
   cancelButton.onmousemove = stop_propagation;
-
-  //elem.innerHTML = '';
-  elem.replaceChildren();
-  elem.appendChild(nameLabel);
-  elem.appendChild(nameInput);
-  elem.appendChild(document.createElement('br'));
-  elem.appendChild(descLabel);
-  elem.appendChild(descInput);
-  elem.appendChild(document.createElement('br'));
-  elem.appendChild(urlLabel);
-  elem.appendChild(urlInput);
-  elem.appendChild(document.createElement('br'));
-  elem.appendChild(saveButton);
   elem.appendChild(cancelButton);
+
   window.getSelection().removeAllRanges();
+}
+
+function add_textbox(elem, id, value, labelText){
+  const input = document.createElement('input');
+  input.id = id;
+  input.type = 'text';
+  input.size = '20';
+  input.value = value;
+  input.onmousedown = stop_propagation;
+
+  const label = document.createElement('label');
+  label.htmlFor = id;
+  label.innerText = labelText;
+
+  elem.appendChild(label);
+  elem.appendChild(input);
+  elem.appendChild(document.createElement('br'));
+  return input;
 }
 
 function save(event, elem){
@@ -432,13 +362,9 @@ function save(event, elem){
   const {desc, url, name} = elem.urlData;
   const saveButton = event.target;
 
-  const newName = saveButton.nameInput.value;
-  const newDesc = saveButton.descInput.value;
-  const newUrl = saveButton.urlInput.value;
-
-  elem.urlData.name = newName;
-  elem.urlData.url = newUrl;
-  elem.urlData.desc = newDesc;
+  elem.urlData.name = saveButton.nameInput.value;
+  elem.urlData.desc = saveButton.descInput.value;
+  elem.urlData.url = saveButton.urlInput.value;
 
   render_content(elem);
   post_url_data();
@@ -451,4 +377,15 @@ function cancel(event, elem){
 
 function stop_propagation(event){
   event.stopPropagation();
+}
+
+function post_url_data() {
+  const urlData = query_url_data();
+  console.log(`Writing data: ${urlData}`);
+  const url = "http://main:8080/video_urls";
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(urlData),
+    headers: { "Content-Type": "application/json" }
+  });
 }
